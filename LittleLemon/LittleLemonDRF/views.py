17f3b5153aaaf9a0035926e9cb9bd112199d9d3d
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-18 13:54:17 -0700

    Import classes from models, serializers and generics. create a generics class to display data with a queryset

--- LittleLemon/LittleLemonDRF/views.py
+++ LittleLemon/LittleLemonDRF/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework import generics
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer()
