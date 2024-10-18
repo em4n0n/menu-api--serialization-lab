eda74e92c2b2aee1c8422a084496422eb9d47ad5
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-18 14:00:32 -0700

    Create another class to view single menu items

--- LittleLemon/LittleLemonDRF/views.py
+++ LittleLemon/LittleLemonDRF/views.py
@@ -6,3 +6,7 @@ from rest_framework import generics
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer()
+    
+class SingleMenuItemview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer()
