f1ea34261438fffbdbb3804b25ad3680498ae520
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-18 14:01:24 -0700

    Make migrations

--- LittleLemon/LittleLemonDRF/views.py
+++ LittleLemon/LittleLemonDRF/views.py
@@ -7,6 +7,6 @@ class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer()
     
-class SingleMenuItemview(generics.RetrieveUpdateDestroyAPIView):
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer()
