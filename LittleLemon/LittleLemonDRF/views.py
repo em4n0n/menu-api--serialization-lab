d9a21c708bd64097b4447c83f4adb21ad627a083
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-19 22:09:14 -0700

    Update MenuItemSerializer path

--- LittleLemon/LittleLemonDRF/views.py
+++ LittleLemon/LittleLemonDRF/views.py
@@ -5,8 +5,8 @@ from rest_framework import generics
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer()
+    serializer_class = MenuItemSerializer
     
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer()
+    serializer_class = MenuItemSerializer
