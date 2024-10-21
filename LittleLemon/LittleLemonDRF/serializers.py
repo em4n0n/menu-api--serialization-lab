ff8b55bc2670e45619bbdf41f702cc9cfe125888
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-20 21:22:12 -0700

    Import bleach and write a validate_title funciton

--- LittleLemon/LittleLemonDRF/serializers.py
+++ LittleLemon/LittleLemonDRF/serializers.py
@@ -1,11 +1,15 @@
-from .models import MenuItem
 from rest_framework import serializers
-
+from .models import MenuItem
+import bleach
 class MenuItemSerializer(serializers.ModelSerializer):
+    def validate_title(self, value):
+        return bleach.clean(value)
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'inventory']
+        fields = [
+            'id', 'title', 'price', 'inventory'
+        ]
         extra_kwargs = {
-                'price': {'min_value': 2},
-                'inventory': {'min_value': 0}
+            'price':{'min_value': 2},
+            'inventory':{'min_value': 0}
         }
